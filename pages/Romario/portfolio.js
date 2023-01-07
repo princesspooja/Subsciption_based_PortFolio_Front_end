ee6e9123b340cd26a1c53bab3451c46774172e9b
shriNiwash <shriniwashsharma98413@gmail.com>
2023-01-07 21:57:31 +0545

    the herosection remamed as Romario

---
+++ pages/Romario/portfolio.js
@@ -0,0 +1,32 @@
+import Head from "next/head";
+import Script from "next/script";
+
+const Portfolio = () => {
+    return (<>
+        <Head>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" />
+
+        </Head>
+        <body>
+            <form>
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                </div>
+                <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </body>
+
+    </>)
+}
+
+export default Portfolio;
--- pages/herosection/portfolio.js
+++
@@ -1,7 +0,0 @@
-const Portfolio = () =>{
-    return(<>
-    <h1 className="main">Hello from Portfolio page</h1>
-    </>)
-}
-
-export default Portfolio;
