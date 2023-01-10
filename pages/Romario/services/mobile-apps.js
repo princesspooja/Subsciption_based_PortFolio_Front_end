79dc37233f5008c69111d625bfe82ec00af85efb
shriNiwash <shriniwashsharma98413@gmail.com>
2023-01-10 22:48:15 +0545

    the error of atpos is resolved

--- pages/Romario/services/mobile-apps.js
+++ pages/Romario/services/mobile-apps.js
@@ -43,10 +43,10 @@ const Mobile_apps = () =>
 
         <div className="summerynote">
 
-            There are many variations of passages of Lorem Ipsum available, but the majority have suffered
+        There are many variations of passages of Lorem Ipsum available, but the majority have suffered
             alteration in
-            some form, by injected humour, or randomised words which don't look even slightly believable. If you are
-            going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in
+            some form, by injected humour, or randomised words which donot look even slightly believable. If you are
+            going to use a passage of Lorem Ipsum, you need to be sure there isnot anything embarrassing hidden in
             the
             middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as
             necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin
