ac1047ab9a3b00764eedf737f966bc777ff52954
princesspooja <poojakushwaha2075@gmail.com>
2023-01-10 21:33:02 +0545

    services page has been made by pooja

--- pages/Romario/portfolio.js
+++ pages/Romario/portfolio.js
@@ -7,7 +7,7 @@ const Portfolio = () => {
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" />
 
         </Head>
-        <body>
+        
             <form>
                 <div className="mb-3">
                     <label for="exampleInputEmail1" className="form-label">Email address</label>
@@ -24,7 +24,7 @@ const Portfolio = () => {
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
-        </body>
+        
 
     </>)
 }
--- pages/Romario/services.js
+++
@@ -1,7 +0,0 @@
-const Services = () =>{
-    return(<>
-    <h1 className="main">Hello from Services page</h1>
-    </>)
-}
-
-export default Services;
