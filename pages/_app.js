c9ce980f105a7355bb1cd2c1ade153b90879bd2e
SaifAliAnsari786 <saifaliansari477@gmail.com>
2023-02-03 14:26:42 +0545

    the dashboard file has benn pushed

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,10 @@ import '../styles/userdashboard/dashboard.css'
 import '../styles/userdashboard/editprofile.css'
 import '../styles/userdashboard/cutomdomain.css'
 import '../styles/userdashboard/homesection.css'
+import '../styles/userdashboard/testimonials.css'
+import '../styles/userdashboard/changepassword.css'
+import '../styles/userdashboard/uploadcv.css'
+import '../styles/userdashboard/addvcard.css'
 
 
 
