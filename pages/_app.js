037e7d16ea248d9f06b2205b31fb4634e96310f5
SaifAliAnsari786 <saifaliansari477@gmail.com>
2023-02-16 14:23:43 +0545

    intial code pused

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,36 @@
 import '../styles/globals.css'
 import '../styles/userdashboard/dashboard.css'
+import '../styles/userdashboard/editprofile.css'
+import '../styles/userdashboard/cutomdomain.css'
+import '../styles/userdashboard/homesection.css'
+import '../styles/userdashboard/testimonials.css'
+import '../styles/userdashboard/changepassword.css'
+import '../styles/userdashboard/uploadcv.css'
+import '../styles/userdashboard/addvcard.css'
+import '../styles/userdashboard/favicon.css'
+import '../styles/userdashboard/table.css'
+import '../styles/userdashboard/services.css'
+import '../styles/userdashboard/packagelist.css'
+
+//admin dashboard css..
+import '../styles/adminDashboard/allsubdomain.css'
+import '../styles/adminDashboard/pendingsubdomain.css'
+import '../styles/adminDashboard/connectsubdomain.css'
+import '../styles/adminDashboard/registeruser.css'
+import '../styles/adminDashboard/favicon.css'
+import '../styles/adminDashboard/logo.css'
+import '../styles/adminDashboard/mailfromadmin.css'
+import '../styles/adminDashboard/mailtoadmin.css'
+import '../styles/adminDashboard/maintenancemode.css'
+import '../styles/adminDashboard/basicinformations.css'
+
+// admin user
+import '../styles/adminUser/profiles.css'
+
+
+
+
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
